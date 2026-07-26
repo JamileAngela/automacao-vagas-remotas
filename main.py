e5d2fc2e83d708e884625b0d6751df0fb7bfdac7
jamileangela <jamile_sousa@outlook.com>
2026-07-26 16:57:49 -0300

    Primeiro teste de conexão online

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Robô de vagas iniciado com sucesso!")
+import requests
+
+print("Robô de vagas iniciado!")
+
+url = "https://api.github.com/repos/python/cpython"
+
+resposta = requests.get(url)
+
+dados = resposta.json()
+
+print("Projeto encontrado:")
+print(dados["name"])
+print("Estrelas:")
+print(dados["stargazers_count"])
