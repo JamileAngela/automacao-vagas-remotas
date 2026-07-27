7222c5f8d26f91728ae2f85e2810ce9a6bc981fc
jamileangela <jamile_sousa@outlook.com>
2026-07-26 22:05:02 -0300

    Adicionar classificador de vagas

--- classificador.py
+++ classificador.py
@@ -1,1 +1,28 @@
+from perfis import PERFIS
 
+
+def classificar_vaga(vaga):
+
+    texto = (
+        vaga.get("titulo", "") +
+        " " +
+        vaga.get("descricao", "")
+    ).lower()
+
+    resultados = []
+
+    for perfil, palavras in PERFIS.items():
+
+        pontos = 0
+
+        for palavra in palavras:
+            if palavra.lower() in texto:
+                pontos += 1
+
+        if pontos > 0:
+            resultados.append({
+                "perfil": perfil,
+                "pontuacao": pontos
+            })
+
+    return resultados
