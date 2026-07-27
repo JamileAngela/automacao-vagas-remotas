ba8910eaaf4ad447d070fda243dd98e2f92c1f27
jamileangela <jamile_sousa@outlook.com>
2026-07-26 23:02:48 -0300

    Update limpeza.py

--- limpeza.py
+++ limpeza.py
@@ -1,1 +1,14 @@
+def remover_duplicadas(vagas):
 
+    vagas_unicas = []
+    links = set()
+
+    for vaga in vagas:
+
+        link = vaga.get("redirect_url")
+
+        if link not in links:
+            vagas_unicas.append(vaga)
+            links.add(link)
+
+    return vagas_unicas
